fa7b0a4ea4aa3b4eac391338c7f276ae6d9f0974
Maksim Peskov <kostarsf@gmail.com>
2025-09-13 12:52:17 +0300

    Enhance error handling in main function and ensure proper formatting in package.json

--- src/index.tsx
+++ src/index.tsx
@@ -5,22 +5,30 @@ import { select } from "@inquirer/prompts";
 import { findDayModules } from "./lib/find-day-modules";
 
 const main = async () => {
-    const modules = await findDayModules(join(import.meta.dir, "days"));
+    try {
+        const modules = await findDayModules(join(import.meta.dir, "days"));
 
-    if (modules.length === 0) {
-        console.error("No modules found");
-        return;
+        if (modules.length === 0) {
+            console.error("No modules found");
+            return;
+        }
+
+        const selectedModule = await select({
+            message: "Select day to run",
+            choices: modules.map((module) => ({
+                name: module.name,
+                value: module,
+            })),
+        });
+
+        await selectedModule.execute();
+    } catch (error) {
+        if (error instanceof Error && error.name === "ExitPromptError") {
+            return;
+        }
+
+        throw error;
     }
-
-    const selectedModule = await select({
-        message: "Select day to run",
-        choices: modules.map((module) => ({
-            name: module.name,
-            value: module,
-        })),
-    });
-
-    await selectedModule.execute();
 };
 
 await main();
