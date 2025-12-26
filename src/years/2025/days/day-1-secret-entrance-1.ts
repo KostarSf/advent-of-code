46102fa89de0df4715886b91971a321a6ffeed18
Maksim Peskov <kostarsf@gmail.com>
2025-12-26 14:49:07 +0300

    Implement Day 1: Secret Entrance solution with input parsing and password decoding. Enhance logging to display password and step values during processing.

--- src/years/2025/days/day-1-secret-entrance-1.ts
+++ src/years/2025/days/day-1-secret-entrance-1.ts
@@ -1,5 +1,36 @@
 export const name = "Day 1: Secret Entrance";
 
 export default async (input: string) => {
-    console.log(input);
+    const sequence = parseSequence(input);
+    const password = decodePassword(sequence);
+    console.log("password: ", password);
 };
+
+const parseSequence = (input: string) =>
+    input
+        .split("\n")
+        .filter(Boolean)
+        .map((line) => (line.startsWith("L") ? -1 : 1) * Number(line.slice(1)));
+
+const decodePassword = (sequence: number[]) => {
+    let value = 50;
+    let zeroesCount = 0;
+    console.log(value);
+    for (const step of sequence) {
+        value = rotateValue(value, step);
+        const displayStep = step > 0 ? `+${step}` : step.toString();
+        if (value === 0) {
+            zeroesCount++;
+            console.log("0", "\t ", displayStep, "\t <-- ", zeroesCount, "total");
+        } else {
+            console.log(value, "\t ", displayStep)
+        }
+    }
+    return zeroesCount;
+}
+
+const rotateValue = (value: number, step: number) => {
+    const sign = Math.sign(step);
+    const absoluteStep = Math.abs(step) % 100;
+    return (value + sign * absoluteStep) % 100;
+}
